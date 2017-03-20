e99eb6702c455e4688185d38835254afb2c00b7a
Paulo Ricardo Xavier Giusti <prxg22@gmail.com>
2017-03-20 12:39:15 -0300

    fixing tel validation

--- segundo-desafio/src/js/directives/TelDirective.js
+++ segundo-desafio/src/js/directives/TelDirective.js
@@ -6,7 +6,7 @@
  * Validate telephone field. It accepts (XX)XXXX-XXXX or (XX)XXXXX-XXXX  only
  */
 (function(app) {
-	var TEL_REGEX = /^(\([0-9]{2}\)||[0-9]{2})[ ]?[0-9]{4}[0-9]?-?[0-9]{4}$/g; // regex to be tested
+	var TEL_REGEX = /^(\([0-9]{2}\)[ ]?||[0-9]{2})[0-9]{4}[0-9]?-?[0-9]{4}$/g; // regex to be tested
 
 	app.directive('tel', function() {
 		return{
